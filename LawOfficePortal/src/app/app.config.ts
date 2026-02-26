d440a0eff6b15cf56d32e4fcbe8dcc451e84895b
dejanefremov <dejan.efremov@vincit.fi>
2026-02-26 14:40:44 +0100

    Update config to set the token

--- LawOfficePortal/src/app/app.config.ts
+++ LawOfficePortal/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { BrowserCacheLocation, IPublicClientApplication, PublicClientApplication, InteractionType } from '@azure/msal-browser';
-import { MsalModule, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MsalGuardConfiguration, MsalService, MsalGuard, MsalBroadcastService } from '@azure/msal-angular';
+import { MsalModule, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MsalGuardConfiguration, MsalService, MsalGuard, MsalBroadcastService, MsalInterceptor, MSAL_INTERCEPTOR_CONFIG, MsalInterceptorConfiguration } from '@azure/msal-angular';
+
 
 import { routes } from './app.routes';
+import { API_BASE_URL } from './constants/api.constants';
 
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
@@ -28,12 +30,41 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
   };
 }
 
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+
+  const scopes = ['api://a9a5990c-f11e-49df-a582-a2c1416456cf/access_as_user'];
+  const addProtectedResource = (baseUrl: string): void => {
+    const normalizedBaseUrl = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+    protectedResourceMap.set(normalizedBaseUrl, scopes);
+    protectedResourceMap.set(`${normalizedBaseUrl}/*`, scopes);
+  };
+
+  addProtectedResource(API_BASE_URL.OFFICE_MANAGEMENT);
+  addProtectedResource(API_BASE_URL.PARTY_MANAGEMENT);
+  addProtectedResource(API_BASE_URL.CASE_MANAGEMENT);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  };
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
     importProvidersFrom(MsalModule),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
     {
       provide: MSAL_INSTANCE,
       useFactory: MSALInstanceFactory,
