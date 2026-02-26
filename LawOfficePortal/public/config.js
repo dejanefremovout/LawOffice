ae9b93467e652701bd8f6dc58ace962569d98876
dejanefremov <dejan.efremov@vincit.fi>
2026-02-26 16:46:52 +0100

    Adjust config

--- LawOfficePortal/public/config.js
+++ LawOfficePortal/public/config.js
@@ -3,5 +3,6 @@ window.__env = {
     OFFICE_MANAGEMENT: 'http://localhost:7206/api',
     PARTY_MANAGEMENT: 'http://localhost:7207/api',
     CASE_MANAGEMENT: 'http://localhost:7208/api'
-  }
+  },
+  REDIRECT_URL: 'http://localhost:4200'
 };
